f71a66c4beeba7774ed4dbb8f0eac6c289b0ad13
Navindu Dananga <navindudananga123@gmail.com>
2024-08-27 01:00:43 +0530

    Change primary color

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -12,6 +12,11 @@ const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
   },
+  palette: {
+    primary: {
+      main: "#6200ED",
+    },
+  }
 });
 
 export default theme;
